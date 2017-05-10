21df54e23a030d57c65945946f288f67f3f83e4f
Ricardo (Rick) (xonar) Nardini <xonar@hotmail.it>
2017-05-11 00:38:36 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1192,12 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"lowecraft.it", "ufodnsseed.ns0.it"},
+    {"lowecraft.it", "ufodnsseed.lowecraft.it"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"lowecraft.it", "testnet-ufodnsseed.ns0.it"},
+    {"lowecraft.it", "tn-ufodnsseed.lowecraft.it"},
     {NULL, NULL}
 };
 
